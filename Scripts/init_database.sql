e0026b1e287eae7dad5dcb70e4b6d16c6ed466f3
Oladapo Lijadu <lijaduoladapobayilori@gmail.com>
2025-06-13 09:20:19 +0100

    Update init_database.sql

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -1,3 +1,13 @@
+/*
+==============================
+Create DATABASE & SCHEMAS
+==============================
+Script purpose: This script create the database 
+"DataWarehouse" the script also set-up schemas
+which are bronze,silver and gold
+
+
+
 -- Create database warehouse
 USE Mysql;
 CREATE DATABASE DataWarehouse;
@@ -7,3 +17,4 @@ USE DataWarehouse;
 CREATE SCHEMA bronze;
 CREATE SCHEMA silver;
 CREATE SCHEMA gold;
+*/
